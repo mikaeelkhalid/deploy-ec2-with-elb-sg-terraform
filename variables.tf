a007d287b1d8f3b871882fb8367b79fed43b5348
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-06-18 15:36:23 +0500

    add variable for vpc, region and profile

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  
+    type = string
+    description = "Vpc id"
+}
+
+variable "region" {
+
+    type = string
+    description = "AWS Region"
+    default = "us-east-1"
+}
+
+variable "profile" {
+
+    type = string
+    description = "AWS Profile"
+    default = "default"
+}
