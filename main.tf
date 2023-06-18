81c715c3ecbe76bc8a476ed0b82904afea4a91f1
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-06-18 16:10:58 +0500

    add sec group

--- main.tf
+++ main.tf
@@ -20,3 +20,25 @@ data "aws_subnets" "default" {
   }
 }
 
+resource "aws_security_group" "secGroup" {
+  name        = "secGroup"
+  description = "Allow Http inbound traffic"
+  vpc_id      = data.aws_vpc.main.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+}
+
+
