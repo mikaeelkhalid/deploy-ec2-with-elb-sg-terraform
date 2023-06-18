3fcd309b14806b0782f21d921e6e8f450dbcf872
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-06-18 16:17:40 +0500

    add ec2 instance and run fmt cmd

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
- terraform {
-   required_providers {
-     aws = {
-       source  = "hashicorp/aws"
-       version = "~> 4.16"
-     }
-   }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
 
-   required_version = ">= 1.2.0"
- }
+  required_version = ">= 1.2.0"
+}
 
- data "aws_vpc" "main" {
+data "aws_vpc" "main" {
   id = var.vpc_id
 }
 
@@ -26,19 +26,27 @@ resource "aws_security_group" "secGroup" {
   vpc_id      = data.aws_vpc.main.id
 
   ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "TCP"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
 
+resource "aws_instance" "default" {
+  ami                         = "ami-0889a44b331db0194"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = ["${aws_security_group.secGroup.id}"]
+  key_name                    = "defaultKeypair"
+  user_data_replace_on_change = true
+  user_data_base64            = "IyEvYmluL2Jhc2gNCnN1ZG8geXVtIHVwZGF0ZSAteQ0Kc3VkbyB5dW0gaW5zdGFsbCAteSBodHRwZA0Kc3VkbyBzeXN0ZW1jdGwgc3RhcnQgaHR0cGQNCnN1ZG8gc3lzdGVtY3RsIGVuYWJsZSBodHRwZA0Kc3VkbyB1c2VybW9kIC1hIC1HIGFwYWNoZSBlYzItdXNlcg0Kc3VkbyBjaG93biAtUiBlYzItdXNlcjphcGFjaGUgL3Zhci93d3cNCnN1ZG8gY2htb2QgMjc3NSAvdmFyL3d3dw0Kc3VkbyBmaW5kIC92YXIvd3d3IC10eXBlIGQgLWV4ZWMgY2htb2QgMjc3NSB7fSBcOw0Kc3VkbyBmaW5kIC92YXIvd3d3IC10eXBlIGYgLWV4ZWMgY2htb2QgMDY2NCB7fSBcOw0Kc3VkbyBlY2hvICI8P3BocCBwaHBpbmZvKCk7ID8+IiA+IC92YXIvd3d3L2h0bWwvcGhwaW5mby5waHA="
 
+}
--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "vpc_id" {
-  
-    type = string
-    description = "Vpc id"
+
+  type        = string
+  description = "Vpc id"
 }
 
 variable "region" {
 
-    type = string
-    description = "AWS Region"
-    default = "us-east-1"
+  type        = string
+  description = "AWS Region"
+  default     = "us-east-1"
 }
 
 variable "profile" {
 
-    type = string
-    description = "AWS Profile"
-    default = "default"
+  type        = string
+  description = "AWS Profile"
+  default     = "default"
 }
