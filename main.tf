8d89eabf911552da8e08200fd988279a5e4e3698
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-06-18 16:34:13 +0500

    add lb module to main

--- main.tf
+++ main.tf
@@ -50,3 +50,22 @@ resource "aws_instance" "default" {
   user_data_base64            = "IyEvYmluL2Jhc2gNCnN1ZG8geXVtIHVwZGF0ZSAteQ0Kc3VkbyB5dW0gaW5zdGFsbCAteSBodHRwZA0Kc3VkbyBzeXN0ZW1jdGwgc3RhcnQgaHR0cGQNCnN1ZG8gc3lzdGVtY3RsIGVuYWJsZSBodHRwZA0Kc3VkbyB1c2VybW9kIC1hIC1HIGFwYWNoZSBlYzItdXNlcg0Kc3VkbyBjaG93biAtUiBlYzItdXNlcjphcGFjaGUgL3Zhci93d3cNCnN1ZG8gY2htb2QgMjc3NSAvdmFyL3d3dw0Kc3VkbyBmaW5kIC92YXIvd3d3IC10eXBlIGQgLWV4ZWMgY2htb2QgMjc3NSB7fSBcOw0Kc3VkbyBmaW5kIC92YXIvd3d3IC10eXBlIGYgLWV4ZWMgY2htb2QgMDY2NCB7fSBcOw0Kc3VkbyBlY2hvICI8P3BocCBwaHBpbmZvKCk7ID8+IiA+IC92YXIvd3d3L2h0bWwvcGhwaW5mby5waHA="
 
 }
+
+module "load_balancer" {
+  source = "./load_balancer"
+
+  vpc_id                                     = data.aws_vpc.main.id
+  load_balancer_name                         = "default"
+  load_balancer_type                         = "application"
+  security_groud_id                          = aws_security_group.secGroup.id
+  subnets_ids                                = data.aws_subnets.default.ids
+  aws_alb_target_group_name                  = "my-alb-target-group"
+  aws_alb_target_group_port                  = 80
+  aws_alb_target_group_protocol              = "HTTP"
+  aws_alb_target_group_type                  = "instance"
+  aws_alb_listener_port                      = 80
+  aws_alb_listener_protocol                  = "HTTP"
+  aws_alb_listener_default_action_type       = "forward"
+  aws_lb_target_group_attachment_port        = 80
+  aws_lb_target_group_attachment_instance_id = aws_instance.default.id
+}
