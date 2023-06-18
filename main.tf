916ae8e24a05d92b9a5bb97cf8178b00f460fc7e
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-06-18 16:08:35 +0500

    retrieve the default vpc configuration

--- main.tf
+++ main.tf
@@ -8,3 +8,15 @@
 
    required_version = ">= 1.2.0"
  }
+
+ data "aws_vpc" "main" {
+  id = var.vpc_id
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
